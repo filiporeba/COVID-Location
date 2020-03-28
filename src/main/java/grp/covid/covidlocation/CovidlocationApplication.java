69d1c450ac3e90866acf9f87f5b1f318386f0db7
filip poreba <filip.poreba22@gmail.com>
2020-03-28 19:33:12 +0100

    Initial commit

---
+++ src/main/java/grp/covid/covidlocation/CovidlocationApplication.java
@@ -0,0 +1,13 @@
+package grp.covid.covidlocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidlocationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidlocationApplication.class, args);
+    }
+
+}
